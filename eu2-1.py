ab888a9663c03e8668620f6674f446ec8db0adcc
Adam Milner <carmiac@gmail.com>
2012-03-09 15:57:24 -0800

    More pythonic fib generator

--- eu2-1.py
+++ eu2-1.py
@@ -1,13 +1,8 @@
-def even_fib(max):
-    prev =1 
-    cur = 1
-    next = 1
-    while next < max:
-        if not (next % 2): 
-            yield next
-        next = prev+cur
-        prev = cur
-        cur = next
+def fib_gen(max):
+    prev, cur = 0, 1
+    while cur < max:
+        yield cur
+        prev, cur = cur, prev + cur
         
-print sum(even_fib(4000000))
+print sum(x for x in fib_gen(4000000) if not(x%2))
 
